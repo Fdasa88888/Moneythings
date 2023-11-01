754e852b9c7293d3ddf83d818c45c7daedfaf159
Fdasa88888 <137032655+Fdasa88888@users.noreply.github.com>
2023-11-01 10:36:15 +0800

    Update Moneythings.js

--- Moneythings.js
+++ Moneythings.js
@@ -1,7 +1,7 @@
 #/
 [rewrite_local]
 https:\/\/api\.revenuecat\.com\/v1\/(subscribers|receipts) url script-response-body https://raw.githubusercontent.com/Fdasa88888/Moneythings/main/Moneythings.js
-
+https:\/\/api\.revenuecat\.com\/v1\/(subscribers|receipts) url script-response-body https://raw.githubusercontent.com/Fdasa88888/Moneythings/main/Moneythings.js
 [mitm]
 hostname = api.revenuecat.com
 
